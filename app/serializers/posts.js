5e2e822390ba3d198f907d36248fad5e6aa2659b
Kot Shrodingera <kot.shrodingera@gmail.com>
2015-10-04 20:48:57 +0300

    fix

--- app/serializers/posts.js
+++
@@ -1,8 +0,0 @@
-import DS from "ember-data";
-
-export default DS.RESTSerializer.extend({
-  isNewSerializerAPI: true,
-  normalizeResponse: function (store, typeClass, hash) {
-    return hash;
-  }
-});
