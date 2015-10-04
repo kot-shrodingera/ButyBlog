3f26ff423c79b0562a487ad071933e3cc39dd9b3
WhiteWolf4768 <ser4768@ya.ru>
2015-10-04 20:41:05 +0300

    NyaKommite

--- app/serializers/comments.js
+++ app/serializers/comments.js
@@ -2,7 +2,8 @@ import DS from "ember-data";
 
 export default DS.RESTSerializer.extend({
   isNewSerializerAPI: true,
-  normalizeResponse: function (store, typeClass, hash) {
-    return hash;
+  normalizeResponse: function(store, typeClass, hash){
+    this._super();
+    console.log(123);
   }
 });
--- app/serializers/posts.js
+++ app/serializers/posts.js
@@ -2,7 +2,7 @@ import DS from "ember-data";
 
 export default DS.RESTSerializer.extend({
   isNewSerializerAPI: true,
-  normalizeResponse: function (store, typeClass, hash) {
+  normalizeResponse: function(store, typeClass, hash){
     return hash;
   }
 });
